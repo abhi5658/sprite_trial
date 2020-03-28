3bc3f6db6248e65ffd7417089fd85309f1dc5add
abhi5658 <abhishekhemendrashah@gmail.com>
2020-03-29 03:48:52 +0530

    full code commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## Sprite trial
+
+### Steps
+- `npm init -y`
+- `npm install express ejs stripe --save`
+- `npm install dotenv --save-dev`
+- `npm i nodemon --save-dev`
+- create `server.js`
+- create `items.json`
+- create `.env` for storing public/private stripe keys
+- clone basic store website `https://github.com/WebDevSimplified/Introduction-to-Web-Development.git` and take out unecessary files/folders
+- duplicate `store.html` to `store.ejs`
+- express use serve static files
+- create `/store` endpoint to serve `store.ejs`
+- call checkout script in store page
+- add js code in page to get total items and quantity
+- add stripeHandler to launch payment details screen
+- collect token and fetch `/purchase` endpoint and use express json()
+- initialise stripe with secret private key
+- create `/purchase` endpoint
+- add stripe charge code to use token to create charge
---
+++ server.js
@@ -0,0 +1,68 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const stripe_secret_key = process.env.STRIPE_SECRET_KEY
+const stripe_public_key = process.env.STRIPE_PUBLIC_KEY
+
+// console.log(stripe_public_key)
+
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+const stripe = require('stripe')(stripe_secret_key)
+
+const PORT = 3000 || process.env.PORT
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+app.get('/store', function(req, res)  {
+    fs.readFile('items.json', function(error, data){
+        if(error){
+            res.sendStatus(500).end()
+        } else {
+            res.render('store.ejs',{
+                stripePublicKey : stripe_public_key,
+                items : JSON.parse(data)
+            })
+        }
+    })
+})
+
+app.post('/purchase', function(req, res) {
+    fs.readFile('items.json', function(error, data){
+        if(error){
+            console.log('reading failed')
+            res.sendStatus(500).end()
+        } else {
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            // console.log(req.body.items)
+            req.body.items.forEach(function(item){
+                const itemJson = itemsArray.find(function(i) {
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quantity
+            })
+
+            stripe.charges.create({
+                amount: total,
+                source : req.body.stripeTokenId,
+                currency : 'inr'
+            }).then(function() {
+                console.log('Charge successful')
+                res.json({message : 'Successfully purchased items'})
+            })
+            .catch(function(error) { 
+                console.log('charging failed: ')
+                res.sendStatus(500).end()
+            })
+        }
+    })
+})
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
